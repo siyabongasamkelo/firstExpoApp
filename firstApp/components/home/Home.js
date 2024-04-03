9c19a2d9580a51aef11fc89b1ac31773ad189ab1
siyabonga <siyabongasamkelociam@gmail.com>
2024-04-03 12:27:12 -0700

    created welcome page

--- firstApp/components/home/Home.js
+++ firstApp/components/home/Home.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { BackgroundImage } from "./Home.styled";
+import { BackgroundImage, Overlay, TextContainer } from "./Home.styled";
+import { Header } from "../common/Text";
+import { Button, ButtonText } from "../common/Buttons";
 
 const Home = () => {
   return (
-    <BackgroundImage
-      source={require("../../assets/img/home.jpg")}
-    ></BackgroundImage>
+    <BackgroundImage source={require("../../assets/img/home.jpg")}>
+      <Overlay />
+      <TextContainer>
+        <Header>The Most Luxurious Watches On The Planet</Header>
+        <Button>
+          <ButtonText>Shop Now</ButtonText>
+        </Button>
+      </TextContainer>
+    </BackgroundImage>
   );
 };
 
