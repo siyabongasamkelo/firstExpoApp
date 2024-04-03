d4f156aabb2831637be6526fa73edf88e9ed239d
siyabonga <siyabongasamkelociam@gmail.com>
2024-04-03 12:28:41 -0700

    feat: added welcome page styles

--- firstApp/components/home/Home.styled.js
+++ firstApp/components/home/Home.styled.js
@@ -1,14 +1,25 @@
 import styled from "styled-components";
-import { ImageBackground } from "react-native";
-
-export const HomeWrapper = styled.View`
-  height: 100%;
-  width: 100%;
-`;
+import { ImageBackground, View } from "react-native";
 
 export const BackgroundImage = styled(ImageBackground)`
   height: 100%;
   width: 100%;
   background-repeat: no-repeat;
   background-size: cover;
+`;
+
+export const Overlay = styled.View`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+`;
+
+export const TextContainer = styled(View)`
+  width: 90%;
+  margin-left: 5%;
+  margin-top: 145%;
+  text-align: center;
 `;
