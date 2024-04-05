aef3d3e8d25cd82acfb7fa04eea7cdddf4ae3277
siyabonga <siyabongasamkelociam@gmail.com>
2024-04-05 10:26:32 -0700

    fchore: navigation folder for all navigation components

---
+++ firstApp/navigation/BottomTabNavigation.js
@@ -0,0 +1,78 @@
+import { View, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../components/home/Home";
+import { Ionicons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+const screenOptions = {
+  tabBarShowLabel: false,
+  tabBarHideOnKeyboard: true,
+  headerShown: false,
+  tabBarStyle: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    elevation: 0,
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    height: 60,
+  },
+};
+
+const BottomNavigation = () => {
+  return (
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                name={focused ? "home" : "home-outline"}
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            );
+          },
+        }}
+      />
+
+      <Tab.Screen
+        name="Search"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                name={focused ? "search" : "search-outline"}
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            );
+          },
+        }}
+      />
+
+      <Tab.Screen
+        name="Profile"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                name={focused ? "person" : "person-outline"}
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomNavigation;
