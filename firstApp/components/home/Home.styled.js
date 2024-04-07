dc131c7f79780f462e038ef9aa4ea16ae0d37450
siyabonga <siyabongasamkelociam@gmail.com>
2024-04-07 16:44:44 -0700

    feat: added some styling

--- firstApp/components/home/Home.styled.js
+++ firstApp/components/home/Home.styled.js
@@ -1,25 +1,7 @@
 import styled from "styled-components";
-import { ImageBackground, View } from "react-native";
 
-export const BackgroundImage = styled(ImageBackground)`
-  height: 100%;
-  width: 100%;
-  background-repeat: no-repeat;
-  background-size: cover;
-`;
-
-export const Overlay = styled.View`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.6);
-`;
-
-export const TextContainer = styled(View)`
+export const Wrapper = styled.View`
+  height: 90%;
   width: 90%;
-  margin-left: 5%;
-  margin-top: 145%;
-  text-align: center;
+  margin: 5% 0 0 5%;
 `;
