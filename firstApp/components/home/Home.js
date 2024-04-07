c4debc3774c7ed734f6c23f2d4e706908e8b8f11
siyabonga <siyabongasamkelociam@gmail.com>
2024-04-07 16:42:41 -0700

    feat: created homepage and added some few elements

--- firstApp/components/home/Home.js
+++ firstApp/components/home/Home.js
@@ -1,19 +1,11 @@
-import React from "react";
-import { BackgroundImage, Overlay, TextContainer } from "./Home.styled";
-import { Header } from "../common/Text";
-import { Button, ButtonText } from "../common/Buttons";
-
+import { Wrapper } from "./Home.styled";
+import { MediumHeader, P } from "../common/Text";
 const Home = () => {
   return (
-    <BackgroundImage source={require("../../assets/img/home.jpg")}>
-      <Overlay />
-      <TextContainer>
-        <Header>The Most Luxurious Watches On The Planet</Header>
-        <Button>
-          <ButtonText>Shop Now</ButtonText>
-        </Button>
-      </TextContainer>
-    </BackgroundImage>
+    <Wrapper>
+      <MediumHeader>All Watches</MediumHeader>
+      <P>The best watches ever...</P>
+    </Wrapper>
   );
 };
 
