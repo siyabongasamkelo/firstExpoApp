d46f6ee1817835082b869d0b0586181dd5eb44c8
siyabonga <siyabongasamkelociam@gmail.com>
2024-04-08 11:50:59 -0700

    feat: added search box, safeAreaView and Item Carousel

--- firstApp/components/home/Home.js
+++ firstApp/components/home/Home.js
@@ -1,11 +1,18 @@
 import { Wrapper } from "./Home.styled";
 import { MediumHeader, P } from "../common/Text";
+import SearchBox from "../common/SearchBox";
+import { SafeAreaView } from "react-native";
+import ItemCarousel from "../common/Carousel";
 const Home = () => {
   return (
-    <Wrapper>
-      <MediumHeader>All Watches</MediumHeader>
-      <P>The best watches ever...</P>
-    </Wrapper>
+    <SafeAreaView>
+      <Wrapper>
+        <MediumHeader>All Watches</MediumHeader>
+        <P>The best watches ever...</P>
+        <SearchBox />
+        <ItemCarousel />
+      </Wrapper>
+    </SafeAreaView>
   );
 };
 
