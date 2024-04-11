a3a936800081af9179449c89fac8b21f6c735a76
siyabonga <siyabongasamkelociam@gmail.com>
2024-04-11 11:20:55 -0700

    fix: uncommented lot of code and fix the problem of elements squashing each other

--- firstApp/components/home/Home.js
+++ firstApp/components/home/Home.js
@@ -1,18 +1,76 @@
-import { Wrapper } from "./Home.styled";
+import {
+  AllItems,
+  ItemCartegories,
+  ScrollableView,
+  SmallBtnText,
+  SmallButton,
+  Wrapper,
+} from "./Home.styled";
 import { MediumHeader, P } from "../common/Text";
 import SearchBox from "../common/SearchBox";
 import { SafeAreaView } from "react-native";
 import ItemCarousel from "../common/Carousel";
+import { useState } from "react";
+import ItemCard from "../common/ItemCard";
+
 const Home = () => {
+  const [btnOneClick, setBtnOneClick] = useState(true);
+  const [btnTwoClick, setBtnTwoClick] = useState(false);
+  const [btnThreeClick, setBtnThreeClick] = useState(false);
+
+  const btnClickHandler = (number) => {
+    if (number === 1) {
+      setBtnOneClick(true);
+      setBtnTwoClick(false);
+      setBtnThreeClick(false);
+    }
+    if (number === 2) {
+      setBtnTwoClick(true);
+      setBtnOneClick(false);
+      setBtnThreeClick(false);
+    }
+    if (number === 3) {
+      setBtnThreeClick(true);
+      setBtnOneClick(false);
+      setBtnTwoClick(false);
+    }
+  };
   return (
-    <SafeAreaView>
-      <Wrapper>
-        <MediumHeader>All Watches</MediumHeader>
-        <P>The best watches ever...</P>
-        <SearchBox />
-        <ItemCarousel />
-      </Wrapper>
-    </SafeAreaView>
+    <ScrollableView>
+      <SafeAreaView>
+        <Wrapper>
+          <MediumHeader>All Watches</MediumHeader>
+          <P>The best watches ever...</P>
+          <SearchBox />
+          <ItemCartegories>
+            <SmallButton
+              clicked={btnOneClick}
+              onPress={() => btnClickHandler(1)}
+            >
+              <SmallBtnText clicked={btnOneClick}>All</SmallBtnText>
+            </SmallButton>
+            <SmallButton
+              clicked={btnTwoClick}
+              onPress={() => btnClickHandler(2)}
+            >
+              <SmallBtnText clicked={btnTwoClick}>Popular</SmallBtnText>
+            </SmallButton>
+            <SmallButton
+              clicked={btnThreeClick}
+              onPress={() => btnClickHandler(3)}
+            >
+              <SmallBtnText clicked={btnThreeClick}>Recent</SmallBtnText>
+            </SmallButton>
+          </ItemCartegories>
+          <ItemCarousel />
+
+          <AllItems>
+            <ItemCard></ItemCard>
+            <ItemCard></ItemCard>
+          </AllItems>
+        </Wrapper>
+      </SafeAreaView>
+    </ScrollableView>
   );
 };
 
