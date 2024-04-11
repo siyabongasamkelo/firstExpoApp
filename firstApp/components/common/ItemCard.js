4e93a3ac84aee2cf83b7eaa5d36964981cc25d85
siyabonga <siyabongasamkelociam@gmail.com>
2024-04-11 11:56:18 -0700

    feat: item card for displaying items in a fancy way

---
+++ firstApp/components/common/ItemCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ItemCardWrapper } from "./ItemCard.styled";
+
+const ItemCard = () => {
+  return <ItemCardWrapper></ItemCardWrapper>;
+};
+
+export default ItemCard;
