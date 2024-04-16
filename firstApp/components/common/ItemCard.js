ed654b49bed3193d53219bed6c54d6d4e2526fb4
siyabonga <siyabongasamkelociam@gmail.com>
2024-04-16 11:34:30 -0700

    feat: added product details like price & name

--- firstApp/components/common/ItemCard.js
+++ firstApp/components/common/ItemCard.js
@@ -1,8 +1,30 @@
 import React from "react";
-import { ItemCardWrapper } from "./ItemCard.styled";
+import {
+  ItemBtnText,
+  ItemButton,
+  ItemCardWrapper,
+  ItemPrice,
+  ItemText,
+  PriceAndHeart,
+  StyledImage,
+} from "./ItemCard.styled";
+import { Ionicons } from "@expo/vector-icons";
 
 const ItemCard = () => {
-  return <ItemCardWrapper></ItemCardWrapper>;
+  return (
+    <ItemCardWrapper>
+      <StyledImage source={require("../../assets/img/item.jpg")} />
+      <ItemText>Golden Milano</ItemText>
+      <PriceAndHeart>
+        <ItemPrice>$199.99</ItemPrice>
+        <Ionicons name="heart" size={24} color="red" />
+      </PriceAndHeart>
+      <ItemButton>
+        <ItemBtnText>Add to cart</ItemBtnText>
+        <Ionicons name="bag" size={24} color="rgba(255,255,255, 0.7)" />
+      </ItemButton>
+    </ItemCardWrapper>
+  );
 };
 
 export default ItemCard;
