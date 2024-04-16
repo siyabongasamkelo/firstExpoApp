43ef49b312b2bf90331461a1ace4d58129d24221
siyabonga <siyabongasamkelociam@gmail.com>
2024-04-16 11:42:20 -0700

    feat: added homeHeader

--- firstApp/components/home/Home.styled.js
+++ firstApp/components/home/Home.styled.js
@@ -20,7 +20,6 @@ export const ItemCartegories = styled.View`
 export const SmallButton = styled(Button)`
   height: 30px;
   width: auto;
-  background-color: #898121;
   background-color: ${(props) => (props.clicked ? "#898121" : "transparent")};
   padding-left: 20px;
   padding-right: 20px;
@@ -28,7 +27,6 @@ export const SmallButton = styled(Button)`
 `;
 
 export const SmallBtnText = styled(P)`
-  color: rgba(255, 255, 255, 0.8);
   color: ${(props) =>
     props.clicked ? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)"};
 `;
@@ -39,8 +37,18 @@ export const ScrollableView = styled(ScrollView)`
 
 export const AllItems = styled.View`
   width: 98%;
-  /* height: 50px; */
-  flex: 1;
-  background-color: blue;
-  margin: 30% 0 0 2%;
+  margin: 3% 0 0 2%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+export const HomeHeader = styled.View`
+  height: 60px;
+  width: 100%;
+  margin-top: 5%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
