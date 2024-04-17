faa885508f60ff4afe7ae7714093f3a23244c838
siyabonga <siyabongasamkelociam@gmail.com>
2024-04-17 10:27:51 -0700

    feat: added SmallHeader

--- firstApp/components/common/Text.js
+++ firstApp/components/common/Text.js
@@ -12,11 +12,15 @@ export const MediumHeader = styled(Text)`
   font-size: 22px;
   font-weight: 600;
   color: rgba(0, 0, 0, 0.8);
-  /* margin-top: 5%; */
+`;
+
+export const SmallHeader = styled(Text)`
+  font-size: 16px;
+  font-weight: 400;
+  color: rgba(0, 0, 0, 0.8);
 `;
 
 export const P = styled(Text)`
   font-size: 14px;
-  /* color: rgba(255, 255, 255, 0.8); */
   color: ${({ color }) => color || "rgba(0, 0, 0, 0.6)"};
 `;
